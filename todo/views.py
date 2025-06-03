db4c7fb51c4c40bcec0665abdc7f591fa567da9b
shuankao <shuanshuankao1222@gmail.com>
2025-06-03 17:22:17 +0800

    完成首頁最新消息

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
+def todolist(request):
+    todos=Todo.objects.all()
+    result={"todos":todos}
+    return render(request,"todo/todolist.html",result)
