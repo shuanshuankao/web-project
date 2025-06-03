f20005307e9f08f22a91e3057941dd7e6d885acb
shuankao <shuanshuankao1222@gmail.com>
2025-06-03 15:11:26 +0800

    新增views.py邏輯跟urls.py綁定

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
+# Create your views here.
+
+def hello(request):
+    result={"message":"測試!","data":123,"label":"文字標籤"}
+    return JsonResponse(result)
